ada1d720018ec45301c0d89215c1f7e4aa7294c9
jonathanfrancisco <jonathanfrancisco999@gmail.com>
2018-06-24 04:56:43 +0800

    Project init

---
+++ config/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    dbHost: 'localhost',
+    dbUsername: 'root',
+    dbPassword: 'mathematics',
+    dbName: 'poll_app',
+    serverPort: process.env || 3000,
+    sessionSecret: 'cats'
+}
