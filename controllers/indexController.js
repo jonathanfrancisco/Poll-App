7d89c8f02f73ef05d84be8a705efca3c8fe09790
jonathanfrancisco <jonathanfrancisco999@gmail.com>
2018-06-26 01:29:09 +0800

    Add register page

---
+++ controllers/indexController.js
@@ -0,0 +1,9 @@
+
+
+module.exports.showRegisterPage = (req,res) => {
+    res.render('register');
+}
+
+module.exports.registerUser = (req, res) => {
+    // login the user dude!!
+}
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-
+const indexController = require('../controllers/indexController.js');
 router.get('/', (req, res) => {
     res.redirect('/polls');
 });
+
+router.get('/register', indexController.showRegisterPage);
+router.post('/register', indexController.registerUser);
 
 module.exports = router;
