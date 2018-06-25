94a364aa7fdb5c1d9b71f4d017f0aff1796c545f
jonathanfrancisco <jonathanfrancisco999@gmail.com>
2018-06-26 00:31:53 +0800

    Add polls page pagination

--- controllers/pollController.js
+++ controllers/pollController.js
@@ -6,9 +6,17 @@ module.exports.getPolls = async (req, res) => {
     try {
 
         const connection = await mysql.getConnection();
-        const polls = await connection.query('SELECT * FROM poll');
+
+        const itemsPerPage = 12;
+        const offset = !req.params.page ? 1 : req.params.page;
+        connection.query('DELETE FROM poll WHERE title = ?',['PRIII~']);
+        const totalRecords = await connection.query('SELECT count(*) AS count FROM poll');
+        const polls = await connection.query(`SELECT * FROM poll ORDER BY created_at DESC LIMIT ${itemsPerPage} OFFSET ${offset} `);
+        connection.end();
         res.render('polls', {
-            polls: polls[0]
+            polls: polls[0],
+            currentPage: offset,
+            pages: Math.ceil(totalRecords[0][0].count / itemsPerPage)
         });
 
     } catch(err) {
--- routes/poll.js
+++ routes/poll.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const pollController = require('../controllers/pollController.js');
 
+
 router.get('/', pollController.getPolls);
+router.get('/:page', pollController.getPolls);
 
 
 
